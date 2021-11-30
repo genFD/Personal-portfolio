3ade4ad8d9d8e6e53199464b47806c4f81534f39
hermkan <hermann.kanga.dev@gmail.com>
2021-11-29 21:14:51 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        body: '#64ffda',
+        body: '#17171f',
+        'selected text': '#A3A3FF',
+        theme: '#3f3fff',
+        nav: '#3f3fff',
+        secondary: '#9191a4',
+        badge: '#3f3f51',
+        'input-border': '#565666',
+        input: '#2a2a35',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
